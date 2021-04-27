3ac0d8fbad878277f11eefef0d71dd50f179a15e
Eli Hopkins <ewh3157@rit.edu>
2021-04-27 02:38:12 -0400

    Fixed Issue Preventing Program from Being Ran without Arguments

--- java/Presentation.java
+++ java/Presentation.java
@@ -170,7 +170,7 @@ public class Presentation extends JFrame {
     public static void main(String[] args) {
     	dLayer = new DataLayer();
 
-        if (args[0].equals("1")) {
+        if (args.length != 0 && args[0].equals("1")) {
             pLayer = new Presentation(1);
         } else {
             pLayer = new Presentation(0);
